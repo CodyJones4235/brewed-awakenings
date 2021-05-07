31bc2bcafef3ad69e19a7c2a5641149e27b454b2
CodyJones4235 <codyjones4325@gmail.com>
2021-05-07 11:27:23 -0500

    first commit

---
+++ scripts/Employees.js
@@ -0,0 +1,37 @@
+import { getEmployees, getOrders } from "./database.js"
+
+const orders = getOrders()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const employeeSales = orders.filter(order => {
+                        if (order.employeeId === employee.id) {
+                            return true
+                        }
+                    })
+                    window.alert(`${employee.name} sold ${employeeSales.length} products`)
+                }
+            }
+        }
+    }
+)
+
+const employees = getEmployees()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
